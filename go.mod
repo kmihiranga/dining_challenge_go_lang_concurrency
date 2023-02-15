e398119258ac7df164f78462fb295eebdc68a0ed
kalanamihiranga-aet <kalana.m@aeturnum.com>
2023-02-15 23:02:01 +0530

    dining challenge with tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dining_challenge
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Five philosophers dine together at the same table. Each philosopher has their own place at the table. There is a fork between each plate. The dish served is a kind of spaghetti which has to be eaten with two forks. Each philosopher can only alternately think and eat. Moreover, a philosopher can only eat their spaghetti when they have both a left and right fork. Thus two forks will only be available when their two nearest neighbors are thinking, not eating. After an individual philosopher finishes eating, they will put down both forks. The problem is how to design a regimen (a concurrent algorithm) such that no philosopher will starve
+
+// Philosopher is a struct which stores information about a philosopher
+type Philosopher struct {
+	name      string
+	rightFork int
+	leftFork  int
+}
+
+// list of all Philosophers
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotal", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Locke", leftFork: 3, rightFork: 4},
+}
+
+// define some variables
+var hunger = 3 // how many time does a person eat
+var eatTime = 1 * time.Second
+var thinkTime = 1 * time.Second
+var sleepTime = 1 * time.Second
+
+var orderMutex sync.Mutex  // a mutex for the slice order finished. part of challenge
+var orderFinished []string // the order in which philosopher finish dining and leave; part of challenge
+
+func main() {
+	// print out a welcome message
+	fmt.Println("Dining Philosophers problem")
+	fmt.Println("---------------------------")
+	fmt.Println("The table is empty.")
+
+	// start the meal
+	dine()
+
+	// print out the finished message
+	fmt.Println("The table is empty.")
+}
+
+func dine() {
+	// eatTime = 0 * time.Second
+	// sleepTime = 0 * time.Second
+	// thinkTime = 0 * time.Second
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all five forks
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal
+	for i := 0; i < len(philosophers); i++ {
+		// fire off a go routine for the current philospher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+
+	// seat the Philosopher at the table
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+	seated.Done()
+
+	seated.Wait()
+
+	// eat three times
+	for i := hunger; i > 0; i-- {
+		// get a lock on both forks
+		if philosopher.leftFork > philosopher.rightFork {
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+		} else {
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+		}
+
+		fmt.Printf("\t%s has both forks and both eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		fmt.Printf("\t%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		forks[philosopher.leftFork].Unlock()
+		forks[philosopher.rightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", philosopher.name)
+
+	}
+
+	fmt.Println(philosopher.name, "is satisfied.")
+	fmt.Println(philosopher.name, "left the table.")
+
+	orderMutex.Lock()
+	orderFinished = append(orderFinished, philosopher.name)
+	orderMutex.Unlock()
+}
