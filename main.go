9f05365669f6b3476bf9616e30d382204a30f665
kalanamihiranga_aet <kalana.m@aeturnum.com>
2023-02-16 20:22:09 +0530

    minor change

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 	"time"
 )
 
-// Five philosophers dine together at the same table. Each philosopher has their own place at the table. There is a fork between each plate. The dish served is a kind of spaghetti which has to be eaten with two forks. Each philosopher can only alternately think and eat. Moreover, a philosopher can only eat their spaghetti when they have both a left and right fork. Thus two forks will only be available when their two nearest neighbors are thinking, not eating. After an individual philosopher finishes eating, they will put down both forks. The problem is how to design a regimen (a concurrent algorithm) such that no philosopher will starve
+// Five philosophers dine together at the same table. Each philosopher has their own place at the table.
+//There is a fork between each plate. The dish served is a kind of spaghetti which has to be eaten with two forks.
+//Each philosopher can only alternately think and eat.
+//Moreover, a philosopher can only eat their spaghetti when they have both a left and right fork.
+//Thus two forks will only be available when their two nearest neighbors are thinking, not eating.
+//After an individual philosopher finishes eating, they will put down both forks.
+//The problem is how to design a regimen (a concurrent algorithm) such that no philosopher will starve
 
 // Philosopher is a struct which stores information about a philosopher
 type Philosopher struct {
